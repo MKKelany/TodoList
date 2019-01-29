1781f534ab31d5e9f103601166d4cb7212bbc61b
Mohab Kelany <mohabkh.kelany@gmail.com>
2019-01-29 14:23:21 +0200

    Build Basic HTML and Logic

---
+++ assets/js/todolist.js
@@ -0,0 +1,21 @@
+//check an li (ToDo)
+$('ul').on('click', "li", function(){
+   $(this).toggleClass("completed");
+});
+//Remove TODO Button
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    }); 
+    e.stopPropagation();
+});
+//If User Entered a new toDO 
+$("input[type=text]").on("keypress",function(e){
+    if(e.which === 13){
+        //catch input string when user hit enter 
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X<span> "+todoText+"</li>");
+
+    }
+})
